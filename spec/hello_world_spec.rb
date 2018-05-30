fc540c6f3e96f46a50e90f6094ce31eab81f82eb
Atul Khanduri <atul91khanduri@gmail.com>
2018-05-30 23:38:32 +0530

    Fixed Rspec

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -5,6 +5,6 @@ RSpec.feature "Contact", :type => :feature do
   scenario "Create a new contact" do
     visit "/"
 
-    expect(page).to have_text("Helloo")
+    expect(page).to have_text("Hello")
   end
 end
